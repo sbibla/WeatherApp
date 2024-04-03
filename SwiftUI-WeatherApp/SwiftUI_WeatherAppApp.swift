c12b95454f6e171bb0fc534d3c1ed3203c88f84b
sbibla <saar.bibla@gmail.com>
2024-04-02 23:30:59 -0700

    Initial Commit

---
+++ SwiftUI-WeatherApp/SwiftUI_WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherAppApp.swift
+//  SwiftUI-WeatherApp
+//
+//  Created by Saar Bibla on 4/2/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
